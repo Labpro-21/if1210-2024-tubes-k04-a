c9761804224599fc47ade27e2a409fd22388e40e
Refki Alfarizi <refkialfarizi123@gmail.com>
2024-05-02 00:01:16 +0700

    feat: add FileIO.py as a module to read and write csv files

--- src/FileIO.py
+++ src/FileIO.py
@@ -1,14 +1,18 @@
 import os
 
-DIR_NAME = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+# Path direktori parent (path folder repositori)
+DIR_PATH = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
-def read(file_name: str) -> list[dict[str, str]]:
-    file_name = DIR_NAME + "/data/" + file_name
-    with open(file_name, "r") as f:
-        lines = f.readlines()
-    parsed_lines = []
-    for line in lines:
-        parsed_lines.append(parse(line))
+def read_csv(folder_name: str, file_name: str) -> list[dict[str, str]]:
+    """
+    Membaca data dari file csv yang ditentukan dan mengembalikannya sebagai list dari dictionary.
+    """
+    file_path = f"{DIR_PATH}/data/{folder_name}/{file_name}"
+    text = ""
+    with open(file_path, "r") as f:
+        text = f.read()
+
+    parsed_lines = _parse(text)
     
     keys = parsed_lines[0]
     formatted_data = []
@@ -21,38 +25,52 @@ def read(file_name: str) -> list[dict[str, str]]:
     
     return formatted_data
 
-def write(file_name: str, data: list[dict[str, str]]):
-    file_name = DIR_NAME + "/data/" + file_name
+def write_csv(folder_name:str, file_name: str, data: list[dict[str, str]]):
+    """
+    Menulis data ke file csv yang ditentukan.
+    """
+    file_path = f"{DIR_PATH}/data/{folder_name}/{file_name}"
     
     data_csv = []
-    data_csv.append(to_csv(list(data[0].keys())))
+    data_csv.append(_to_csv(list(data[0].keys())))
 
     for i in range(len(data)):
-        data_csv.append(to_csv(list(data[i].values())))
+        data_csv.append(_to_csv(list(data[i].values())))
 
-    with open(file_name, 'w') as f:
+    with open(file_path, 'w') as f:
         for i in range(len(data_csv)):
             f.write(data_csv[i])
 
 
-
-def parse(data: str) -> list[str]:
-    parsed = []
+def _parse(data: str) -> list[list[str]]:
+    """
+    Melakukan parsing string csv dan mengembalikan list dari list data per baris.
+    """
+    parsed_lines = []
+    line = []
     temp = ""
     for i in range(len(data)):
         if data[i] == ';':
-            parsed.append(temp)
+            line.append(temp)
             temp = ""
         elif data[i] == "\n":
-            break
+            line.append(temp)
+            temp = ""
+            parsed_lines.append(line)
+            line = []
         else:
             temp += data[i]
     
     if temp: 
-        parsed.append(temp)
-    return parsed
+        line.append(temp)
+    if line:
+        parsed_lines.append(line)
+    return parsed_lines
 
-def to_csv(data: list[str]) -> str:
+def _to_csv(data: list[str]) -> str:
+    """
+    Mengubah list dari string menjadi string dengan format csv.
+    """
     joined = ""
     for i in range(len(data)):
         joined += data[i]
@@ -63,10 +81,10 @@ def to_csv(data: list[str]) -> str:
 
     return joined
 
-if __name__ == "__main__":
-    x = read("test.csv")
+if __name__ == "__main__": # Hanya akan dieksekusi jika dijalankan secara langsung dan bukan sebagai modul
+    x = read_csv("test_folder", "test.csv")
     print(x)
 
-    write("test2.csv", x)
+    write_csv("test_folder", "test2.csv", x)
 
     
